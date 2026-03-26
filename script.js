db0ba2d76f86f60ca5d757d6aac5c2f84138a299
emmanuelansong <62494346+emmanuelansong@users.noreply.github.com>
2026-03-26 20:23:09 +0000

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function scrollToProjects() {
-  document.getElementById('projects').scrollIntoView({
-    behavior: 'smooth'
-  });
-}
-
-function openProject(page) {
-  window.location.href = page;
-}
